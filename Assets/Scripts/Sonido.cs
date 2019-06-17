7d7024679baf0b11f373dbdffcec4369823f1a5c
MateoMena <mateoccsj@gmail.com>
2019-06-17 02:40:54 -0300

    Incorporamos un sonido general loopeable para la pantalla de inicio

---
+++ Assets/Scripts/Sonido.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Sonido : MonoBehaviour {
+
+	public GameObject reproducirMusica;
+	public GameObject detenerMusica;
+	AudioSource reproductor;
+	void Start () {
+
+		reproducirMusica.SetActive(false);
+		detenerMusica.SetActive(true);
+
+		reproductor = this.gameObject.GetComponent<AudioSource>();
+	}
+	
+	void Update(){
+
+	}
+
+	public void Reproducir(){
+		reproducirMusica.SetActive(false);
+		detenerMusica.SetActive(true);
+
+		reproductor.Play();
+	}
+	public void Detener(){
+		reproducirMusica.SetActive(true);
+		detenerMusica.SetActive(false);
+
+		reproductor.Pause();
+	}
+
+}
+	
+
+
