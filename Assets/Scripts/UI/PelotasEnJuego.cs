649631594a3c477d138dfbb6cf9bd0977245faf1
aleramirezsj <aleramirezsj@gmail.com>
2019-07-03 00:41:40 -0300

    fix en sliders de cantidad de pelotas y resaltadas

---
+++ Assets/Scripts/UI/PelotasEnJuego.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PelotasEnJuego : MonoBehaviour {
+
+	public Text TxtCantidadPelotas;
+	public Text TxtCantidadResaltadas;
+	private int cantidadPelotasActual;
+	public Slider sldCantidadPelotas;
+	public Slider sldCantidadResaltadas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sldCantidadPelotas.value!=cantidadPelotasActual)
+		{
+			cantidadPelotasActual=(int)sldCantidadPelotas.value;
+			TxtCantidadPelotas.text=cantidadPelotasActual.ToString();
+
+			if(ComportamientoPelota.cantidadResaltadas == cantidadPelotasActual) {
+				//Restamos 1 a la propiedad estatica 'cantidadResaltadas'
+				ComportamientoPelota.cantidadResaltadas--;
+				//Asignamos la propiedad estatica 'cantidadResaltadas' al slider.
+				sldCantidadResaltadas.value = ComportamientoPelota.cantidadResaltadas;
+				//Asignamos el valor del la propiedad al texto.
+				TxtCantidadResaltadas.text = ComportamientoPelota.cantidadResaltadas.ToString();
+			}
+		}
+	}
+
+	
+
+}
---
+++ Assets/Scripts/UI/PelotasResaltadas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PelotasResaltadas : MonoBehaviour {
+
+	public Text TxtCantidadResaltadas;
+	public Slider sldCantidadResaltadas;
+	public Slider sldCantidadPelotas;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//definimos que la actualización de la cantidad de resaltadas se realice solo cuando sea menor a la cantidad de pelotas definidas
+		if (sldCantidadResaltadas.value!=ComportamientoPelota.cantidadResaltadas && sldCantidadPelotas.value>sldCantidadResaltadas.value)
+		{
+			//asignamos la cantidad de pelotas resaltadas que tiene el slider a la propiedad estática "cantidadResaltadas" de la clase
+			//ComportamientoPelota
+			ComportamientoPelota.cantidadResaltadas=(int)sldCantidadResaltadas.value;
+			//actualizamos el número de pelotas resaltadas en la pantalla
+			TxtCantidadResaltadas.text=ComportamientoPelota.cantidadResaltadas.ToString();
+        }
+	}
+
+	
+
+}
